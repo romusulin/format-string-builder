81ed0882b2896709b16cfb7f8be5486d558a4473
Roko Musulin <roko.musulin@gmail.com>
2018-09-02 17:04:33 +0200

    Initial commit

---
+++ src/format-string-builder.ts
@@ -0,0 +1,64 @@
+declare const module;
+
+class FormatStringBuilder {
+	private nl: string = "\n";
+	private str: string;
+
+	constructor(str: string = "") {
+		this.str = str;
+	}
+
+	get length(): number {
+		return this.str.length;
+	}
+
+	get lines(): string | string[] {
+		return this.str.split(this.nl);
+	}
+
+	get newline() {
+		return this.nl;
+	}
+
+	set newline(newline: string) {
+		this.nl = newline;
+	}
+
+	append(str: string): FormatStringBuilder {
+		this.str += str;
+
+		return this;
+	}
+
+	appendLn(str: string = ""): FormatStringBuilder {
+		return this.append(str + this.nl);
+	}
+
+	format(...args: string[]): FormatStringBuilder {
+		const regex: RegExp = /{(\d+)}/gm;
+		let formatString: string = args[0];
+
+		let matches: RegExpExecArray;
+		while ((matches = regex.exec(formatString))) {
+			const matchedString: string = matches[0];
+			const index: number = Number(matches[1]) + 1;
+
+            if (args[index]) {
+                formatString = formatString.replace(matchedString, args[index]);
+            }
+		}
+
+		return this.append(formatString);
+	}
+
+	clear() {
+		this.str = "";
+
+		return this;
+	}
+
+	toString(): string {
+		return this.str;
+	}
+}
+module.exports = FormatStringBuilder;
---
+++ test/test.js
@@ -0,0 +1,49 @@
+var expect = require("chai").expect;
+var FormatStringBuilder = require("../lib/format-string-builder.js");
+describe("Format string builder tests", () => {
+	it ("should format single arg", () => {
+		const Builder = new FormatStringBuilder();
+		Builder.format("Hello {0}", "world");
+
+		expect(Builder.toString()).to.equal("Hello world");
+		expect(Builder.clear().toString()).to.equal("");
+	});
+
+	it ("should format multiple args", () => {
+		const Builder = new FormatStringBuilder();
+		Builder.format("Hello {0}, my name is {1} {2} and i am very {3} to {4} you", "you", "uncle", "bob", "pleased", "meet");
+
+		expect(Builder.toString()).to.equal("Hello you, my name is uncle bob and i am very pleased to meet you");
+		expect(Builder.clear().toString()).to.equal("");
+	});
+
+	it("should append and appendLn", () => {
+		const Builder = new FormatStringBuilder();
+		Builder.append("first line");
+		expect(Builder.toString()).to.equal("first line");
+
+		Builder.append(" ");
+		Builder.append("still first line");
+		expect(Builder.toString()).to.equal("first line still first line");
+
+		Builder.appendLn();
+		Builder.append("second line");
+		expect(Builder.toString()).to.equal("first line still first line\nsecond line");
+
+		expect(Builder.lines.length).to.equal(2);
+		expect(Builder.lines[0]).to.equal("first line still first line");
+		expect(Builder.lines[1]).to.equal("second line");
+
+	});
+
+	it("should get and set newline", () => {
+		const Builder = new FormatStringBuilder();
+		expect(Builder.newline).to.equal("\n");
+
+		Builder.newline = "rakija";
+		expect(Builder.newline).to.equal("rakija");
+	});
+});
+
+
+
